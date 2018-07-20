2dcc1a00dded837c8e0af9f95e0304afeea434d8
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-07-20 18:43:48 +0200

    [WIP] Adding Passport

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from 'body-parser';
 import router from './controllers';
 import * as express from 'express';
+const passport = require('./config/auth')();
 
 const app: express.Application = express();
 app.set('port', process.env.PORT || 3001);
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(passport.initialize());
 
 router(app);
 
