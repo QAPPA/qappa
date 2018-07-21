690bf7838a668932fe4124a9a0ebd668d96d8cda
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-07-21 09:22:34 +0200

    Refactored login to be in a controller

--- server/src/controllers/authenticationController.ts
+++ server/src/controllers/authenticationController.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 import { getRepository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import * as _ from 'lodash';
 import * as config from 'config';
 import { User } from '../entities/User';
+const router = Router();
 
-export default async (req: Request, res: Response) => {
+router.post('/login', async (req: Request, res: Response) => {
     // TODO: replace with some robust validation - Joi?
     if (!req.body.email || !req.body.password) {
         return res.sendStatus(400);
@@ -22,4 +23,6 @@ export default async (req: Request, res: Response) => {
     }
     const token = jwt.sign(_.pick(user, ['id', 'admin']), config.get('jwtSecret'));
     res.status(200).send(token);
-};
+});
+
+export default router;
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,13 +1,13 @@
 import { Application, Request, Response } from 'express';
 import usersController from './usersController';
-import login from './login';
+import authenticationController from './authenticationController';
 import { authenticate } from '../middleware/auth';
 
 export default (app: Application) => {
     // unprotected endpoints:
     // POST /login
     // POST /users
-    app.post('/login', login);
+    app.use('/login', authenticationController);
     app.use('/users', usersController);
     app.get('/protected', authenticate(), (req: Request, res: Response) => {
         console.log('Inside protected');
