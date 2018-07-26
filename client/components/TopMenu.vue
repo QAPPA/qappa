6dcd9a1a74d13a5583ccc33d4bcef3f2bf635dc0
Vladimír Fojtík <fojtik.v@gmail.com>
2018-07-26 07:52:22 +0200

    Added logged user text into top menu

--- client/components/TopMenu.vue
+++ client/components/TopMenu.vue
@@ -8,6 +8,9 @@
             active-text-color="#409EFF"
             @select="handleSelect">
             <el-menu-item index="1">Home</el-menu-item>
+            <div class="menu-text">
+                <p class="menu-user"><strong>Logged user:</strong> {{ user }}</p>
+            </div>
         </el-menu>
         <div class="line"></div>
     </div>
@@ -20,6 +23,11 @@
                 activeIndex: '1'
             };
         },
+        computed: {
+            user() {
+                return this.$auth.user;
+            }
+        },
         methods: {
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
@@ -27,3 +35,15 @@
         }
     };
 </script>
+
+<style lang="sass" scoped>
+    .menu-text
+        float: right
+        color: #fff
+    .menu-user
+        height: 60px
+        line-height: 60px
+        margin: 0
+        font-size: 14px
+        padding: 0 20px
+</style>
