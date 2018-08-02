ee0db205d42b7c6b305dee742d434ae80e40a8e6
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-08-02 16:09:30 +0200

    Grouped login tests

--- server/tests/controllers/authenticationController.test.ts
+++ server/tests/controllers/authenticationController.test.ts
@@ -37,41 +37,40 @@ describe('POST /auth/login', () => {
         }
     });
 
-    it('should respond with status 400 if invalid body was supplied', async () => {
-        // uses validate() method which has already been tested
-        const response: Response = await request
-            .post('/auth/login')
-            .send({
-                email: 'this is not a valid email',
-                password: ''
-            });
-        expect(response.status).toBe(400);
-        expect(response.body.message).toMatch(/Validation error/);
+    describe('should respond with status code 400', () => {
+        it('if invalid body was supplied', async () => {
+            // uses validate() method which has already been tested
+            const response: Response = await request
+                .post('/auth/login')
+                .send({
+                    email: 'this is not a valid email',
+                    password: ''
+                });
+            expect(response.status).toBe(400);
+            expect(response.body.message).toMatch(/Validation error/);
+        });
+        it('if user was not found', async () => {
+            const response: Response = await request
+                .post('/auth/login')
+                .send({
+                    email: 'nonexistent@qappa.net',
+                    password: 'password'
+                });
+            expect(response.status).toBe(400);
+            expect(response.body.message).toMatch(/Invalid email or password/);
+        });
+        it('if user password doesn\'t match', async () => {
+            const response: Response = await request
+                .post('/auth/login')
+                .send({
+                    email: 'test@qappa.net',
+                    password: 'wrongPassword'
+                });
+            expect(response.status).toBe(400);
+            expect(response.body.message).toMatch(/Invalid email or password/);
+        });
     });
-
-    it('should respond with status 400 if user was not found', async () => {
-        const response: Response = await request
-            .post('/auth/login')
-            .send({
-                email: 'nonexistent@qappa.net',
-                password: 'password'
-            });
-        expect(response.status).toBe(400);
-        expect(response.body.message).toMatch(/Invalid email or password/);
-    });
-
-    it('should respond with status 400 if user password doesn\'t match', async () => {
-        const response: Response = await request
-            .post('/auth/login')
-            .send({
-                email: 'test@qappa.net',
-                password: 'wrongPassword'
-            });
-        expect(response.status).toBe(400);
-        expect(response.body.message).toMatch(/Invalid email or password/);
-    });
-
-    it('should respond with status 200 if login is successful', async () => {
+    it('should respond with status 200 and JWT token in response if login is successful', async () => {
         const response: Response = await request
             .post('/auth/login')
             .send({
@@ -79,15 +78,7 @@ describe('POST /auth/login', () => {
                 password: 'password'
             });
         expect(response.status).toBe(200);
-    });
 
-    it('should send correct JWT token in response if login is successful', async () => {
-        const response: Response = await request
-            .post('/auth/login')
-            .send({
-                email: 'test@qappa.net',
-                password: 'password'
-            });
         const { token } = response.body;
         const payload = await jwt.verify(token, config.get('jwtSecret'));
         expect(payload).toMatchObject({
