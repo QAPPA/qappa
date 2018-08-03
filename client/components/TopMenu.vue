194b02a755efea548a49fc2c028b55970a6cdf83
Vladimír Fojtík <fojtik.v@gmail.com>
2018-08-03 14:57:56 +0200

    Added logout notification

--- client/components/TopMenu.vue
+++ client/components/TopMenu.vue
@@ -47,6 +47,12 @@
         methods: {
             async handleLogout() {
                 await this.$auth.logout();
+                this.$notify({
+                    title: 'Success',
+                    message: 'You have been successfully logged off from system.',
+                    type: 'success',
+                    position: 'bottom-right'
+                });
             }
         }
     };
