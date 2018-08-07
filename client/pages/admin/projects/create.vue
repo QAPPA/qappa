4437e9104e4bdf9e855c7e2b8be11a0c278069ba
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-08-07 23:41:18 +0200

    Fix ElementUI locale, first iteration of create project form

--- client/pages/admin/projects/create.vue
+++ client/pages/admin/projects/create.vue
@@ -3,7 +3,37 @@
         <app-breadcrumb />
         <b-row>
             <b-col>
-                <h1>Project create</h1>
+                <h1>Create a new project</h1>
+                <el-form
+                    ref="form"
+                    :model="form"
+                    :rules="rules"
+                    label-width="150px">
+                    <el-form-item
+                        label="Name"
+                        prop="name">
+                        <el-input
+                            v-model="form.name"
+                            :maxlength="100"></el-input>
+                    </el-form-item>
+                    <el-form-item
+                        label="Deadline"
+                        prop="deadline">
+                        <el-date-picker
+                            v-model="form.deadline"
+                            :picker-options="deadlineOptions"
+                            type="date"
+                            placeholder="Pick a date"
+                            format="dd/MM/yyyy"
+                            value-format="dd/MM/yyyy">
+                        </el-date-picker>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button
+                            type="primary"
+                            @click="handleSubmit">Create</el-button>
+                    </el-form-item>
+                </el-form>
             </b-col>
         </b-row>
     </div>
@@ -19,8 +49,51 @@ export default {
     middleware: 'admin',
     data() {
         return {
-            pageTitle: 'Create a project'
+            pageTitle: 'Create a project',
+            form: {
+                name: '',
+                deadline: '', // in the format dd/MM/yyyy !
+            },
+            deadlineOptions: {
+                disabledDate(time) {
+                    const yesterday = Date.now() - (24 * 60 * 60 * 1000);
+                    return time.getTime() <= yesterday;
+                }
+            },
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: 'Please fill in project name.',
+                        trigger: 'change'
+                    },
+                    {
+                        max: 100,
+                        message: 'Project name must be less than 100 characters long.',
+                        trigger: 'change'
+                    }
+                ],
+                deadline: [
+                    {
+                        required: true,
+                        message: 'Please fill in project deadline.',
+                        trigger: 'change'
+                    }
+                ]
+            }
         };
     },
+    methods: {
+        handleSubmit() {
+            this.$refs.form.validate((valid) => {
+                if (!valid) {
+                    return;
+                }
+                // API call
+                console.log('Calling API with', this.form.name, this.form.deadline);
+                alert('Project created');
+            });
+        }
+    }
 };
 </script>
--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import Element from 'element-ui/lib/element-ui.common';
 import locale from 'element-ui/lib/locale/lang/en';
 
 export default () => {
-    Vue.use(ElementUI);
-    Vue.use(Element, {locale});
+    Vue.use(ElementUI, { locale });
 };
