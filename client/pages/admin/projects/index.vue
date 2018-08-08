f410283a599b06edc4aec4c0a2666569e68f28d2
Lukáš Stuchlík <stuchlik1@seznam.cz>
2018-08-08 02:03:21 +0200

    Project list with simulated data

--- client/pages/admin/projects/index.vue
+++ client/pages/admin/projects/index.vue
@@ -6,6 +6,38 @@
                 <h1>Project</h1>
             </b-col>
         </b-row>
+        <b-row>
+            <b-col>
+                <el-table
+                    :data="projects"
+                    empty-text="No projects found">
+                    <el-table-column
+                        prop="id"
+                        label="ID"
+                        width="50">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="Name"
+                        width="400">
+                    </el-table-column>
+                    <el-table-column
+                        prop="deadline"
+                        label="Deadline"
+                        width="120">
+                    </el-table-column>
+                    <el-table-column
+                        prop="responsible"
+                        label="Responsible person"
+                        width="250">
+                    </el-table-column>
+                    <el-table-column
+                        prop="team"
+                        label="Team members">
+                    </el-table-column>
+                </el-table>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -18,8 +50,96 @@
         },
         data() {
             return {
-                pageTitle: 'Projects'
+                pageTitle: 'Projects',
+                projects: [],
             };
         },
+        mounted() {
+            // simulated API call, probably not accurate, depicting mostly the data transformation
+            const data = [
+                {
+                    id: 0,
+                    name: 'Project 1',
+                    deadline: '27/08/2018',
+                    responsibleUser: {
+                        id: 0,
+                        name: 'Carl',
+                        surname: 'Johnson'
+                    },
+                    users: [
+                        {
+                            user: {
+                                id: 0,
+                                name: 'Carl',
+                                surname: 'Johnson'
+                            },
+                            roles: [
+                                {
+                                    id: 0,
+                                    name: 'Tester'
+                                },
+                                {
+                                    id: 1,
+                                    name: 'Developer'
+                                }
+                            ]
+                        },
+                        {
+                            user: {
+                                id: 1,
+                                name: 'Jessica',
+                                surname: 'Mellow'
+                            },
+                            roles: [
+                                {
+                                    id: 1,
+                                    name: 'Developer'
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    id: 1,
+                    name: 'Project 2',
+                    deadline: '30/04/2019',
+                    responsibleUser: {
+                        id: 1,
+                        name: 'Jessica',
+                        surname: 'Mellow'
+                    },
+                    users: [
+                        {
+                            user: {
+                                id: 1,
+                                name: 'Jessica',
+                                surname: 'Mellow'
+                            },
+                            roles: [
+                                {
+                                    id: 1,
+                                    name: 'Developer'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ];
+            this.projects = data.map(project => {
+                const responsible = `${project.responsibleUser.name} ${project.responsibleUser.surname}`;
+                const team = project.users.map(member => {
+                   const name = `${member.user.name} ${member.user.surname}`;
+                   const roles = member.roles.map(role => role.name).join(',');
+                   return `${name} (${roles})`;
+                }).join(', ');
+                return {
+                    id: project.id,
+                    name: project.name,
+                    deadline: project.deadline,
+                    responsible,
+                    team
+                };
+            });
+        }
     };
 </script>
