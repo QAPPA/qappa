9d03a77d5981fef2180b1dbddcfaeadc3c701b6a
sMteX <stuchlik1@seznam.cz>
2018-08-08 23:32:15 +0200

    Add skeleton of RoleController

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,6 +2,7 @@ import { Application, Request, Response } from 'express';
 import usersController from './usersController';
 import authenticationController from './authenticationController';
 import projectController from './projectController';
+import roleController from './roleController';
 import { authenticate, admin } from '../middleware/auth';
 
 export default (app: Application) => {
@@ -11,6 +12,7 @@ export default (app: Application) => {
     app.use('/auth', authenticationController);
     app.use('/users', usersController);
     app.use('/projects', projectController);
+    app.use('/roles', roleController);
     app.get('/protected', authenticate, (req: Request, res: Response) => {
         console.log('Inside protected');
         return res.sendStatus(200);
