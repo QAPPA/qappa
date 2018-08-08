000b1771aa733986473bbe8bf02d2536cbe0e6a4
sMteX <stuchlik1@seznam.cz>
2018-08-08 18:08:32 +0200

    Added status (opened/closed) to the project table and added a toggle for the status

--- client/pages/admin/projects/index.vue
+++ client/pages/admin/projects/index.vue
@@ -36,14 +36,46 @@
                         label="Team members">
                     </el-table-column>
                     <el-table-column
+                        label="Status">
+                        <template slot-scope="scope">
+                            <span
+                                v-if="scope.row.open"
+                                class="openedProject">
+                                <span class="el-icon-success"></span> Opened
+                            </span>
+                            <span
+                                v-else
+                                class="closedProject">
+                                <span class="el-icon-error"></span> Closed
+                            </span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column
                         label="Operations">
                         <template slot-scope="scope">
-                            <nuxt-link :to="`/admin/projects/edit/${scope.row.id}`">
+                            <nuxt-link
+                                :to="`/admin/projects/edit/${scope.row.id}`"
+                                title="Edit project">
                                 <span class="el-icon-edit"></span>
                             </nuxt-link>
                             <el-button
-                                class="deleteButton"
+                                :title="`${scope.row.open ? 'Close' : 'Open'} project`"
+                                class="operationButton"
                                 type="text"
+                                @click="handleProjectToggle(scope.row.id)">
+                                <span
+                                    v-if="scope.row.open"
+                                    class="el-icon-circle-close-outline">
+                                </span>
+                                <span
+                                    v-else
+                                    class="el-icon-circle-check-outline">
+                                </span>
+                            </el-button>
+                            <el-button
+                                class="operationButton"
+                                type="text"
+                                title="Delete project"
                                 @click="handleProjectDelete(scope.row.id)">
                                 <span class="el-icon-delete"></span>
                             </el-button>
@@ -74,6 +106,7 @@
                     id: 0,
                     name: 'Project 1',
                     deadline: '27/08/2018',
+                    open: true,
                     responsibleUser: {
                         id: 0,
                         name: 'Carl',
@@ -116,6 +149,7 @@
                     id: 1,
                     name: 'Project 2',
                     deadline: '30/04/2019',
+                    open: false,
                     responsibleUser: {
                         id: 1,
                         name: 'Jessica',
@@ -149,6 +183,7 @@
                     id: project.id,
                     name: project.name,
                     deadline: project.deadline,
+                    open: project.open,
                     responsible,
                     team
                 };
@@ -158,6 +193,19 @@
             };
         },
         methods: {
+            handleProjectToggle(id) {
+                const capitalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+                const project = this.projects.find(p => p.id === id);
+                const operation = (project.open) ? 'close' : 'open';
+                this.$confirm(`Are you sure you want to ${operation} this project?`, {
+                    type: 'warning',
+                    confirmButtonText: capitalize(operation),
+                    confirmButtonClass: 'el-button--danger'
+                }).then(() => {
+                    console.log(`${capitalize(operation)} project id`, id);
+                    project.open = !project.open;
+                });
+            },
             handleProjectDelete(id) {
                 this.$confirm('Are you sure you want to delete this project?', {
                     type: 'warning',
@@ -176,10 +224,17 @@
     span.el-icon-edit
         color: orange
         font-size: x-large
-    span.el-icon-delete
+    span.el-icon-delete, span.el-icon-circle-close-outline
         color: red
         font-size: x-large
-    .deleteButton
+    span.el-icon-circle-check-outline
+        color: green
+        font-size: x-large
+    .operationButton
         margin-left: 10px
         padding: 0
+    .openedProject
+        color: green
+    .closedProject
+        color: red
 </style>
